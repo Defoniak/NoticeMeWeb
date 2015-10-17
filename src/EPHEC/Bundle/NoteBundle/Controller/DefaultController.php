ab15d23c6d13c7162d95c6f754e5a9e4b74b407b
quentineggerick <quentin.eggerickx@gmail.com>
2015-10-18 01:34:36 +0200

    Affichage d'une légende à coté de la map! Callbackjavascript tu suces des bites et tu m'as cassé les couilles pendant 2h. Je te baise ta mère et je la bourre par tout les trous.

--- src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
+++ src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EPHECNoteBundle:Default:index.html.twig', array('name' => $name));
+        $tab[]= array('desc'=>'Rendez-vous chez le dentiste!', 'date'=>'16/08/2015 8H30', 'lat'=>50.8504500, 'long'=>4.3487800);
+        $tab[]= array('desc'=>'Rendez vous chez le docteur', 'date'=>'10/11/2015 8H30', 'lat'=>50.541441396370274, 'long'=>4.856913942187475);
+        $tab[]= array('desc'=>'Rendez vous avec la mère de Nico', 'date'=>'12/11/2015 18H30', 'lat'=>50.7608912624449, 'long'=>4.758136789062519);
+        $note = json_encode($tab);
+        return $this->render('EPHECNoteBundle:Default:index.html.twig', array('note' => $note));
     }
 }
