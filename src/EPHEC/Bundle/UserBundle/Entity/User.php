4959e29be4238a7f840bbdfe8ebaf3d5a0dd84df
Martin <martin.c@hotmail.com>
2015-10-19 10:48:11 +0200

    Mise en place de l'userBundle. Tout marche en local.

---
+++ src/EPHEC/Bundle/UserBundle/Entity/User.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace EPHEC\Bundle\UserBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ *
+ */
+class User extends baseUser
+{
+    /**
+     *
+     * @ORM\Column(name="lastName", type="string", length=45, nullable=false)
+     */
+    private $lastname;
+
+    /**
+     *
+     * @ORM\Column(name="firstName", type="string", length=45, nullable=false)
+     */
+    private $firstname;
+
+    /**
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     *
+     * @ORM\ManyToMany(targetEntity="EPHEC\Bundle\NoteBundle\Entity\Alarmgroup", mappedBy="user")
+     */
+    private $group;
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Get group
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+}
