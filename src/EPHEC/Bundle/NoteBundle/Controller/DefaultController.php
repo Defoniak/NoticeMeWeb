df9a288065155e8b517574c7aa68e8698e83494b
quentineggerick <quentin.eggerickx@gmail.com>
2015-10-20 16:15:23 +0200

    amelioration visuelle

--- src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
+++ src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        //usermanager get current user
+        //
         $tab[]= array('desc'=>'Rendez-vous chez le dentiste!', 'date'=>'16/08/2015 8H30', 'lat'=>50.8504500, 'long'=>4.3487800);
         $tab[]= array('desc'=>'Rendez vous chez le docteur', 'date'=>'10/11/2015 8H30', 'lat'=>50.541441396370274, 'long'=>4.856913942187475);
         $tab[]= array('desc'=>'Rendez vous avec la mère de Nico', 'date'=>'12/11/2015 18H30', 'lat'=>50.7608912624449, 'long'=>4.758136789062519);
