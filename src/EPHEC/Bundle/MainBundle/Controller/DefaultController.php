699cf4a72e7406ff521ca86cc1db2f8c7aaa39c1
Martin <martin.c@hotmail.com>
2015-10-24 13:32:16 +0200

    connection android

--- src/EPHEC/Bundle/MainBundle/Controller/DefaultController.php
+++ src/EPHEC/Bundle/MainBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('EPHECMainBundle:Default:index.html.twig');
     }
+
+    public function androidLoginAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+
+        $user = $userManager->findUserBy(array('email' => $_POST['username'],'password' => $_POST['password']));
+        $value = (is_null($user))?0:1;
+
+        return $this->render('EPHECMainBundle:Default:androidlogin.html.twig', array("value"=>$value));
+    }
 }
