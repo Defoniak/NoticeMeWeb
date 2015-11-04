e1c0a29f66dca6b221989658c081d468771c5c5d
quentineggerick <quentin.eggerickx@gmail.com>
2015-11-04 16:52:23 +0100

    début de la liste des archives!

--- src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
+++ src/EPHEC/Bundle/NoteBundle/Controller/DefaultController.php
@@ -10,16 +10,7 @@ class DefaultController extends Controller
 {
     public function indexAction($val=0)
     {
-        //usermanager get current user
-        //
-        $tab[] = array('desc' => 'Rendez-vous chez le dentiste!', 'date' => '16/08/2015 8H30', 'lat' => 50.8504500, 'long' => 4.3487800);
-        $tab[] = array('desc' => 'Rendez vous chez le docteur', 'date' => '10/11/2015 8H30', 'lat' => 50.541441396370274, 'long' => 4.856913942187475);
-        $tab[] = array('desc' => 'Rendez vous avec la mère de Nico', 'date' => '12/11/2015 18H30', 'lat' => 50.7608912624449, 'long' => 4.758136789062519);
-        $tab[] = array('desc' => 'Convention', 'date' => '12/11/2015 18H30', 'lat' => 48.8647964666206, 'long' => 2.363014357812517);
-        $tab[] = array('desc' => 'testtresèloing', 'date' => '12/11/2015 18H30', 'lat' => 53.527320580555646, 'long' => -8.409070368750008);
-        //$note = json_encode($tab);
         $tab2 = array();
-
         $alarm = new Alarm();
         $em = $this->getDoctrine()->getManager();
         $groups = $this->getUser()->getGroup();
@@ -27,18 +18,24 @@ class DefaultController extends Controller
         foreach($groups as $group){
             //$alarms[] = $em->getRepository("EPHECNoteBundle:Alarm")->findBy(array('group'=>$group)); // alarms ==> tableau d'alarmes
             // AJOUT DE CODE EXPLOSION
-            $repository = $this
+            /*$repository = $this
                 ->getDoctrine()
                 ->getManager()
                 ->getRepository('EPHECNoteBundle:Alarm')
-            ;
-            $alarms[] = $repository->findNotArchivedAlarms($group); //EXPLOSION
+            ;*/
+            //$alarms[] = $repository->findNotArchivedAlarms($group); //EXPLOSION
 
-            //FIN D AJOUT DE CODE EXPLOSION
-
+            /** @var  $repository \Doctrine\ORM\EntityManager */
+            $query = $em->getRepository("EPHECNoteBundle:Alarm")->createQueryBuilder('a')
+                ->where('a.deletedAt IS NULL')
+                ->andwhere('a.group = :groups')
+                ->setParameter('groups', $group)
+                ->getQuery();
+            $alarms[]= $query->getResult();
+            //FIN D AJOUT DE CODE EXPLOSION\
         }
         //ajouter un boucle
-        print_r($alarms[0]);
+        //print_r($alarms[0]);
         if(!empty($alarms) && !empty($alarms[0])){
             $empty = 0;
             foreach($alarms[0] as $value){
@@ -144,6 +141,24 @@ class DefaultController extends Controller
 
     }
     public function archiveListAction(){
-        return $this->render('EPHECNoteBundle:Default:archivedList.html.twig');
+        $tab = array();
+        $em = $this->getDoctrine()->getManager();
+        $groups = $this->getUser()->getGroup();
+        $alarms = array();
+        foreach($groups as $group){
+            $alarms[] = $em->getRepository("EPHECNoteBundle:Alarm")->findBy(array('group'=>$group)); // alarms ==> tableau d'alarmes
+        }
+        if(!empty($alarms) && !empty($alarms[0])){
+            $empty = 0;
+            foreach($alarms[0] as $value){
+                $tab[] = array('desc'=>$value->getTitle(),'date'=>date_format($value->getDatealarm(), 'Y-m-d H:i:s'),
+                    'lat'=>$value->getLatitude(),'long'=>$value->getLongitude(), 'id'=>$value->getId(), 'deletedAt'=>$value->getDeletedAt());
+            }
+
+        }
+        else { $empty = 1;}
+        $note = json_encode($tab);
+
+        return $this->render('EPHECNoteBundle:Default:archivedList.html.twig', array('note' => $note, 'empty' => $empty));
     }
 }
